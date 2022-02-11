b6d73411313b1aa848594ff654d63df6e0ac784f
Definitely not xl_83 <98920372+LX8369@users.noreply.github.com>
2022-02-11 22:30:12 +1000

    yhui

--- src/components/NavigationBar/NavigationBar.vue
+++ src/components/NavigationBar/NavigationBar.vue
@@ -21,7 +21,10 @@
             </b-navbar-nav>
             <b-navbar-nav style="margin-top: auto; margin-bottom: auto;" class="ml-auto">
                     <b-button id="loggedInData" style="color: white; margin-right: 5px; margin-top: auto; margin-bottom: auto;"><img id="discordPfp" src=" " style=""/>Currently not logged in</b-button>
-                    <logOut id="logDP" style="display: none;"></logOut>
+                    <b-nav-item-dropdown id="logDP" style="display: none; margin-right: 10px; margin-top: auto; margin-bottom: auto;" id="v-step-0" :text="test" right>
+        <b-dropdown-item @click="logOut()">Log Out</b-dropdown-item>
+      
+      </b-nav-item-dropdown>
                 <b-button v-on:click="login()" id="loginButton" style="margin-right: 4px;border-radius: 0em; border-top-left-radius: 0.25em; border-bottom-left-radius: 0.25em" href="https://discord.com/api/oauth2/authorize?client_id=938552684942880869&redirect_uri=https%3A%2F%2Fs4d-xl83.netlify.app%2F&response_type=token&scope=identify">
                 <b-icon-discord></b-icon-discord>
                 </b-button>
