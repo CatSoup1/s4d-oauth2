7e068c99ba4622c923922abf091b4a9724625213
Definitely not xl_83 <98920372+LX8369@users.noreply.github.com>
2022-02-14 00:41:55 +1000

    owow

--- src/components/NavigationBar/CodeModal.vue
+++ src/components/NavigationBar/CodeModal.vue
@@ -20,28 +20,59 @@ export default {
     }
 }
 </script>
+ <style>
+ .close {
+  font-size: 200% !important;
+  color: #eb4034 !important;
+  opacity: 0.8 !important;
+}
 
-<style>
+.close:hover {
+  font-size: 210% !important;
+  opacitiy: 1 !important;
+}
 
 #code {
-    min-width: 100%;
-    min-height: 100%;
+  min-width: 100%;
+  min-height: 100%;
+  border: 5px solid #70788C;
+  border-radius: 0.5em;
+  color: white !important;
+  font-weight: bold !important;
+  padding: 1em;
+  background-color: #3d445a;
 }
-
+  
 #code-modal .modal-dialog {
-    max-width: 100%;
-    margin: 0;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    height: 100vh;
-    display: flex;
-    position: fixed;
-    z-index: 100000;
-}
-.modal-content {
-		background-color: #343434;
+  max-width: 100%;
+  margin: 0;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  display: flex;
+  position: fixed;
+  z-index: 100000;
 }
 
+.modal-header {
+  border-bottom: 0px solid !important;
+}
+
+.modal-title {
+  color: white;
+  font-family: 'lato', serif;
+  font-size: 150%;
+  text-align: center;
+  text-shadow: 2px 2px 10px #121212;
+}
+
+.modal-content {
+	background-color: #1d2332 !important;
+}
+
+.modal-footer {
+  border-top: 0px solid !important;
+}
 </style>
