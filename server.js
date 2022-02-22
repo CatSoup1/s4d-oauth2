ea53cdd494cdce3958ee2754777e36546515f4a0
Definitely not xl_83 <98920372+LX8369@users.noreply.github.com>
2022-02-22 11:08:48 +1000

    owo

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const WebSocketServer = require("ws")
-const wss = new WebSocketServer({ port: "3000" });
+const wss = new WebSocketServer.Server({ port: 3000 });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function message(data) {
