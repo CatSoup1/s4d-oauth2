56af433554c6aa857892459c1de046c5685190fb
Definitely not xl_83 <98920372+LX8369@users.noreply.github.com>
2022-02-23 23:35:39 +1000

    dsddsssdds

--- src/main.js
+++ src/main.js
@@ -254,7 +254,7 @@ async function getAccessCode() {
 
 const id = "12244535"
 const site = "google.com"
-var encryptedData = CryptoJS.AES.encrypt(id, String(crypKey)).toString();
+var encryptedData = CryptoJS.AES.encrypt(JSON.stringify(id), String(crypKey)).toString();
 const obj = `{
     "${encryptedData}": "${site}"
 }`
