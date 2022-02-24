585628c64ad42fba54ed93b5aefd4d21691db91b
Definitely not xl_83 <98920372+LX8369@users.noreply.github.com>
2022-02-24 15:14:35 +1000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -255,7 +255,7 @@ async function getAccessCode() {
 
 var id = "643287564654386"
 var site = "google.com"
-
+console.log(crypKey)
 function encrypt(text){
   var cipher = crypto.createCipher('aes-256-cbc', String(crypKey))
   var crypted = cipher.update(text,'utf8','hex')
