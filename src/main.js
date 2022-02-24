b3df13b140f14e861e71a6eddc24e4efa6f825b2
Definitely not xl_83 <98920372+LX8369@users.noreply.github.com>
2022-02-24 12:54:55 +1000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -251,7 +251,6 @@ async function getAccessCode() {
                         })
                 })
 }
-const algorithm = 'aes-192-cbc';
 
 var crypt = {
   secret : crypKey,
@@ -261,13 +260,6 @@ var crypt = {
     var cipher = CryptoJS.AES.encrypt(clear, crypt.secret);
     cipher = cipher.toString();
     return cipher;
-  }
- 
-  // (B3) DECRYPT
-  function decrypt(cipher) {
-    var decipher = CryptoJS.AES.decrypt(cipher, crypt.secret);
-    decipher = decipher.toString(CryptoJS.enc.Utf8);
-    return decipher;
   }
 const id = "12244535"
 const site = "google.com"
